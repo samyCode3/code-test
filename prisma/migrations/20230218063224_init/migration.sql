6d9a82916940524743ced89c5fd8cb9d4e245999
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2023-02-20 18:10:33 +0100

    update

--- prisma/migrations/20230218063224_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Wizard" (
-    "id" SERIAL NOT NULL,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT,
-
-    CONSTRAINT "Wizard_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20230219123427_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "elixris" ALTER COLUMN "difficulty" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "spells" ADD CONSTRAINT "spells_wizardId_fkey" FOREIGN KEY ("wizardId") REFERENCES "Wizard"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
